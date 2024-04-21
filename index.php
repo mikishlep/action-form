e6e5a2d7144bfc938aee07f18402eb3f00e5600d
mikishlep <156234919+mikishlep@users.noreply.github.com>
2024-04-21 19:39:51 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<!-- форма обратной связи -->
+<form action="send.php" method="post">
+    <input type="text" name="login" placeholder="Логин" required>
+    <input type="password" name="pass" placeholder="Пароль" required>
+    <input type="email" name="email" placeholder="Почта" required>
+    <input type="tel" name="tel" placeholder="Телефон" required>
+
+    <input type="submit" value="Отправить данные">
+</form>
